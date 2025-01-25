adfb6f09ef421e51abdff3004fd63a7effbe2f20
Ali MARZOUK <ali.marzouk@edifixio.com>
2025-01-25 13:19:26 +0100

    🎉 init commit

---
+++ config/config.py
@@ -0,0 +1,6 @@
+import os
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+OUTPUT_FOLDER = os.path.join(ROOT_DIR, 'output')
+OUTPUT_TMP_FOLDER = os.path.join(ROOT_DIR, 'output')
