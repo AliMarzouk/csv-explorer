042295f21e82e324199a15afcfcb046c333ac11d
Ali MARZOUK <ali.marzouk@edifixio.com>
2025-01-26 16:34:41 +0100

    ✨ add value count by columns

--- config/config.py
+++ config/config.py
@@ -3,4 +3,5 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent
 OUTPUT_FOLDER = os.path.join(ROOT_DIR, 'output')
-OUTPUT_TMP_FOLDER = os.path.join(ROOT_DIR, 'output')
+OUTPUT_TMP_FOLDER = os.path.join(OUTPUT_FOLDER, 'tmp')
+OUTPUT_CSV_FILE = os.path.join(OUTPUT_FOLDER, 'inter.csv')
--- lib/test/main_test.py
+++ lib/test/main_test.py
@@ -1,9 +1,10 @@
 import os
 import csv
+import pandas as pd
+import numpy as np
 
 from lib.test.decorators import custom_test
-from lib.analysis.main import determine_types
-from lib.analysis.main import get_missing_values_indexes
+from lib.analysis.main import determine_types, get_missing_values_indexes, count_values_by_columns
 from config import config
 from lib.utils.constants import ColumnDataType
 
@@ -23,7 +24,7 @@ def test_determine_types(test_data, expected):
     result = determine_types(tmp_file, ',')
     os.remove(tmp_file)
 
-    assert result == expected
+    assert result == expected, "Should return expected result"
     
 @custom_test(
     (
@@ -40,4 +41,23 @@ def test_get_missing_values_indexes(test_data, expected):
     result = get_missing_values_indexes(tmp_file, ',', test_data[1], test_data[2])
     os.remove(tmp_file)
 
-    assert result == expected
+    assert result == expected, "Should return expected result"
+    
+    
+@custom_test(
+    (
+        ([["year","state","month","number","date"], ["1998","Acre","Janeiro","0","1998-01-01"], ["2017","Acre","February","1","2017-01-01"], ["2017","Acre",None,"1","2017-01-01"]], "month"),
+        {"month" : {"Janeiro": 1, "February": 1, np.nan: 1}}
+    )
+)
+def test_count_values_by_columns(test_data, expected):
+    tmp_file = f'{config.OUTPUT_TMP_FOLDER}/tmp.csv';
+    with open(tmp_file, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(test_data[0])
+        
+    result = count_values_by_columns(tmp_file, ',', [test_data[1]])
+    
+    os.remove(tmp_file)
+
+    assert result == expected, "Should return expected result"
